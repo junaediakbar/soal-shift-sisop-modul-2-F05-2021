4bde2eb5a67feddb893c631c183feb269fbc4f02
ArchieOps <jonathanleonardo123@gmail.com>
2021-04-21 12:26:48 +0700

    Add Images Dir and Soal3 E & D

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -54,8 +54,53 @@ char *ans = (char *)calloc(80, sizeof(char));
 	}
     return ans;
 }
- 
-int main() {
+
+// //Buat daKilla (SubSoal D)
+// void myKill(){
+    
+//     char *temp = (char *)calloc(80, sizeof(char));
+//     sprintf(temp, "#!/bin/bash\npkill -9 \"soal3\"\nrm -- \"$0\"");
+    
+//     FILE *fp;
+//     fp = fopen("killer.sh", "a+");
+//     fprintf(fp,"%s", temp);
+//     fclose(fp);
+
+//     char *argv6[] = {"makeItExecutable", "a+x", "killer.sh", NULL};
+//     execv("/usr/bin/chmod", argv6);
+// }
+
+
+//DaKilla v2 (Memadukan 3E dan 3D)
+void myKill(char *argv){
+    
+    char *temp = (char *)calloc(80, sizeof(char));
+    
+    //kalau passing argumen -z di CLI
+    if(argv[1] == 'z'){
+        sprintf(temp, "#!/bin/bash\npkill -9 \"soal3\"\nrm -- \"$0\"");
+    }
+
+    //kalau passing argumen -x di CLI
+    if(argv[1] == 'x'){
+        sprintf(temp, "#!/bin/bash\nkill -9 %d\nrm -- \"$0\"", getppid());
+    }
+    
+    FILE *fp;
+    fp = fopen("killer.sh", "a+");
+    fprintf(fp,"%s", temp);
+    fclose(fp);
+
+    char *argv6[] = {"makeItExecutable", "a+x", "killer.sh", NULL};
+    execv("/usr/bin/chmod", argv6);
+}
+
+int main(int argc, char* argv[]) {
+
+    if (strcmp(argv[1], "-z") != 0 && strcmp(argv[1], "-x") != 0){
+        exit(EXIT_FAILURE);
+    }
+
     pid_t pid, sid;        // Variabel untuk menyimpan PID
 
     pid = fork();     // Menyimpan PID dari Child Process
@@ -86,6 +131,19 @@ int main() {
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
+   
+   //Proses ngebuat executable KillingMachine
+    pid_t killerPid;
+    killerPid = fork();
+
+    if(killerPid < 0){
+        exit(EXIT_FAILURE);
+    }
+
+    if(killerPid == 0 ){
+        // myKill();    //Da Killa V1
+        myKill(argv[1]);
+    }
 
     //program utama
     while (1) {
